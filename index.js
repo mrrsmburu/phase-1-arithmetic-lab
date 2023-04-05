d25130ed2131479e31dfc372eac379a11166e504
mrrsmburu <mrrsmburu@gmail.com>
2023-04-05 15:57:07 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1 ;
+
+let num3 = 28
+let num4 = 6
+let mod = num3 % num4
+
+let numbers =[2,4,6,8,10,12,16,20]
+let max = Math.max(...numbers) ;
